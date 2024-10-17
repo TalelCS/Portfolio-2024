7c82af8da00c7dbd11e1e7781645eb1916dfda5c
Talel Galai <talelgalai@gmail.com>
2024-10-17 16:11:59 +0200

    small fix

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -7,7 +7,7 @@ export function TimelineDemo() {
     return (
       <div>
         <p className="font-bold md:text-4xl text-xl text-white">
-          Première expérience avec l'IA
+          Première expérience avec l&apos;IA
         </p>
         <p className="font-normal text-base text-white"></p>
         <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
