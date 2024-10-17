330769d08b38a2c05cec75f1f1c00e53b86cd5c9
Talel Galai <talelgalai@gmail.com>
2024-10-17 15:26:54 +0200

    changed apos

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -103,19 +103,19 @@ export function TimelineDemo() {
       content: (
         <div>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
-            • J’ai développé un chatbot utilisant l’assistant OpenAI, avec une fonctionnalité de diffusion des réponses via WebSocket, basé sur une base de connaissances téléchargée.
+            • J&apos;ai développé un chatbot utilisant l&apos;assistant OpenAI, avec une fonctionnalité de diffusion des réponses via WebSocket, basé sur une base de connaissances téléchargée.
           </p>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
-            • J’ai implémenté des fonctionnalités de lecture en haute voix et de saisie vocale pour permettre une interaction plus naturelle avec le chatbot, en intégrant des API de synthèse vocale et de reconnaissance vocale.
+            • J&apos;ai implémenté des fonctionnalités de lecture en haute voix et de saisie vocale pour permettre une interaction plus naturelle avec le chatbot, en intégrant des API de synthèse vocale et de reconnaissance vocale.
           </p>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
-            • J’ai amélioré le tableau de bord du chatbot en mettant en place un système d'authentification et en optimisant l’interface pour offrir une meilleure expérience utilisateur.
+            • J&apos;ai amélioré le tableau de bord du chatbot en mettant en place un système d&apos;authentification et en optimisant l&apos;interface pour offrir une meilleure expérience utilisateur.
           </p>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
-            • J’ai utilisé Markdown pour formater les réponses du chatbot, ce qui a permis d'offrir des réponses bien structurées, avec des éléments tels que des listes, des titres et des liens cliquables pour une meilleure lisibilité.
+            • J&apos;ai utilisé Markdown pour formater les réponses du chatbot, ce qui a permis d&apos;offrir des réponses bien structurées, avec des éléments tels que des listes, des titres et des liens cliquables pour une meilleure lisibilité.
           </p>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
-            • J’ai conteneurisé le projet en utilisant Docker, permettant le déploiement sur n'importe quelle machine, et j’ai vérifié la compatibilité mobile en effectuant des tests avec Ngrok.
+            • J&apos;ai conteneurisé le projet en utilisant Docker, permettant le déploiement sur n&apos;importe quelle machine, et j&apos;ai vérifié la compatibilité mobile en effectuant des tests avec Ngrok.
           </p>
         </div>
       ),
@@ -158,22 +158,22 @@ export function TimelineDemo() {
       content: (
         <div>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
-            • J’ai dirigé le développement d'un site vitrine pour Fares Confection, une entreprise textile, en créant une interface utilisateur attrayante et intuitive qui met en valeur les produits et services de l'entreprise.
+            • J&apos;ai dirigé le développement d&apos;un site vitrine pour Fares Confection, une entreprise textile, en créant une interface utilisateur attrayante et intuitive qui met en valeur les produits et services de l&apos;entreprise.
           </p>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
-            • J’ai intégré des fonctionnalités avancées sur le site, y compris un système de gestion des produits et un formulaire de contact, facilitant ainsi l'interaction avec les clients potentiels.
+            • J&apos;ai intégré des fonctionnalités avancées sur le site, y compris un système de gestion des produits et un formulaire de contact, facilitant ainsi l&apos;interaction avec les clients potentiels.
           </p>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
-            • J’ai collaboré étroitement avec le client pour définir ses besoins et attentes, en veillant à ce que le produit final réponde à ses exigences tout en respectant les délais impartis.
+            • J&apos;ai collaboré étroitement avec le client pour définir ses besoins et attentes, en veillant à ce que le produit final réponde à ses exigences tout en respectant les délais impartis.
           </p>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
-            • Pour l'événement de JCI, j’ai conçu et développé un système de réservation en ligne, permettant aux participants de s'inscrire facilement à l'événement et d'effectuer des paiements sécurisés.
+            • Pour l&apos;événement de JCI, j&apos;ai conçu et développé un système de réservation en ligne, permettant aux participants de s&apos;inscrire facilement à l&apos;événement et d&apos;effectuer des paiements sécurisés.
           </p>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
-            • J’ai mis en place une base de données robuste pour gérer les inscriptions et les paiements, garantissant ainsi la sécurité et l'intégrité des données des utilisateurs.
+            • J&apos;ai mis en place une base de données robuste pour gérer les inscriptions et les paiements, garantissant ainsi la sécurité et l&apos;intégrité des données des utilisateurs.
           </p>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
-            • J’ai assuré le déploiement et la maintenance continue des deux projets, en fournissant un support technique et en effectuant des mises à jour régulières pour améliorer les performances et la sécurité.
+            • J&apos;ai assuré le déploiement et la maintenance continue des deux projets, en fournissant un support technique et en effectuant des mises à jour régulières pour améliorer les performances et la sécurité.
           </p>
         </div>
       ),
@@ -216,16 +216,16 @@ export function TimelineDemo() {
       content: (
         <div>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
-            • J’ai développé un questionnaire en ligne dynamique avec des scénarios multiples et une base de données flexible, utilisant JavaScript et Appsmit.
+            • J&apos;ai développé un questionnaire en ligne dynamique avec des scénarios multiples et une base de données flexible, utilisant JavaScript et Appsmit.
           </p>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
-            • J’ai créé un tableau de bord interactif pour centraliser les données des enquêtes en ligne, intégrant des visualisations claires et une mise à jour en temps réel des statistiques.
+            • J&apos;ai créé un tableau de bord interactif pour centraliser les données des enquêtes en ligne, intégrant des visualisations claires et une mise à jour en temps réel des statistiques.
           </p>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
-            • J’ai exploré et lu la documentation de l’outil Appsmith pour la création d’applications web et mobiles, en mettant en avant la rapidité de développement et la flexibilité de la plateforme.
+            • J&apos;ai exploré et lu la documentation de l&apos;outil Appsmith pour la création d&apos;applications web et mobiles, en mettant en avant la rapidité de développement et la flexibilité de la plateforme.
           </p>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal">
-            • J’ai collaboré avec des développeurs expérimentés pour acquérir des connaissances et des compétences en matière de travail d’équipe et de communication efficace.
+            • J&apos;ai collaboré avec des développeurs expérimentés pour acquérir des connaissances et des compétences en matière de travail d&apos;équipe et de communication efficace.
           </p>
         </div>
       ),
@@ -268,13 +268,13 @@ export function TimelineDemo() {
       content: (
         <div>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
-            • J’ai eu l’opportunité d’approfondir mes connaissances en développement tout en participant à la création d’un système de notifications capable d’envoyer des alertes par e-mail et SMS aux clients en utilisant Django, un framework web Python haut de gamme.
+            • J&apos;ai eu l&apos;opportunité d&apos;approfondir mes connaissances en développement tout en participant à la création d&apos;un système de notifications capable d&apos;envoyer des alertes par e-mail et SMS aux clients en utilisant Django, un framework web Python haut de gamme.
           </p>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
-            • J’ai fait la conception d’une API RESTful avec Django REST Framework, facilitant l’échange de données avec d’autres applications.
+            • J&apos;ai fait la conception d&apos;une API RESTful avec Django REST Framework, facilitant l&apos;échange de données avec d&apos;autres applications.
           </p>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal">
-            • J’ai géré des bases de données du système avec PostgreSQL, garantissant un stockage et une récupération des informations efficaces.
+            • J&apos;ai géré des bases de données du système avec PostgreSQL, garantissant un stockage et une récupération des informations efficaces.
           </p>
         </div>
       ),
